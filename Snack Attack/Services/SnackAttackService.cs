3b7adef96b46c3138b8c1b65307bb3d05257d450
Mohammad Malik (malikm44) <malikm44@coventry.ac.uk>
2025-04-09 00:23:04 +0100

    added the description section of the items

--- Snack Attack/Services/SnackAttackService.cs
+++ Snack Attack/Services/SnackAttackService.cs
@@ -6,19 +6,19 @@ public class SnackAttackService
 {
     private static readonly IEnumerable<Models.SnackItem> _fastFood = new List<Models.SnackItem>
     {
-        new Models.SnackItem { Name = "Chicken Burger 1", Image = "burger1.png", Price = 4.5 },
-        new Models.SnackItem { Name = "Chicken Burger 2", Image = "burger2.png", Price = 5.5 },
-        new Models.SnackItem { Name = "Chicken Burger Meal", Image = "burger3.png", Price = 6.5 },
-        new Models.SnackItem { Name = "Cheese Burger", Image = "cheeseburger.png", Price = 5.5 },
-        new Models.SnackItem { Name = "Chicken Wings", Image = "chicken_wings.png", Price = 1.2 },
-        new Models.SnackItem { Name = "Cola", Image = "cola.png", Price = 1 },
-        new Models.SnackItem { Name = "Cola Bottle", Image = "cola_bottle.png", Price = 1.2 },
-        new Models.SnackItem { Name = "Lemon Soda", Image = "soda.png", Price = 1.2 },
-        new Models.SnackItem { Name = "Fries", Image = "french_fries.png", Price = 1.5 },
-        new Models.SnackItem { Name = "Full Chicken", Image = "fried_chicken.png", Price = 12.5 },
-        new Models.SnackItem { Name = "Chicken", Image = "fried_chicken1.png", Price = 2 },
-        new Models.SnackItem { Name = "Mayonnaise", Image = "mayonnaise.png", Price = 0.3 },
-        new Models.SnackItem { Name = "Ketchup", Image = "tomato_sauce.png", Price = 0.3 }
+        new Models.SnackItem { Name = "Chicken Burger 1", Image = "burger1.png", Price = 4.5, Description = "Crispy fried chicken fillet with cheese, lettuce and ketchup." },
+        new Models.SnackItem { Name = "Chicken Burger 2", Image = "burger2.png", Price = 5.5, Description = "Crispy fried chicken fillet, 2 cheese, lettuce and Ketchup."},
+        new Models.SnackItem { Name = "Chicken Burger Meal", Image = "burger3.png", Price = 6.5, Description = " Try the meal combo of Crispy fried chicket fillet, 2 cheese, lettuce and Ketchup with fries and cola."},
+        new Models.SnackItem { Name = "Cheese Burger", Image = "cheeseburger.png", Price = 5.5, Description = "Original American cheese burger"},
+        new Models.SnackItem { Name = "Chicken Wings", Image = "chicken_wings.png", Price = 1.2, Description = "Homemade rispy fried chicken wings"},
+        new Models.SnackItem { Name = "Cola", Image = "cola.png", Price = 1, Description = "Coal can (300ml)"},
+        new Models.SnackItem { Name = "Cola Bottle", Image = "cola_bottle.png", Price = 1.2, Description = "Same taste same cola in bottle (500ml)"},
+        new Models.SnackItem { Name = "Lemon Soda", Image = "soda.png", Price = 1.2, Description = "The authentic sola with the taste of citrus in it for refreshment."},
+        new Models.SnackItem { Name = "Fries", Image = "french_fries.png", Price = 1.5, Description = "Homemade fries"},
+        new Models.SnackItem { Name = "Full Chicken", Image = "fried_chicken.png", Price = 12.5, Description = "Homemade crispy fried full chicken which is coked with homdmade marination and coked till perfection (8 pieces)."},
+        new Models.SnackItem { Name = "Chicken", Image = "fried_chicken1.png", Price = 2, Description = "Homemade crispy fried  chicken legs which is cooked with homemade marination and cooked till perfection. "},
+        new Models.SnackItem { Name = "Mayonnaise", Image = "mayonnaise.png", Price = 0.3, Description = "Original real mayonnaise"},
+        new Models.SnackItem { Name = "Ketchup", Image = "tomato_sauce.png", Price = 0.3, Description = "Ketchup made from fresh tomatos"}
     };
 
   

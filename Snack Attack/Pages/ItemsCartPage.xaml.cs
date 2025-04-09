31cb3fc856c2db08502c555a9f835cd6be874908
Mohammad Malik (malikm44) <malikm44@coventry.ac.uk>
2025-04-09 07:40:45 +0100

    added binding context to the ItemsCartPage.xaml.cs

--- Snack Attack/Pages/ItemsCartPage.xaml.cs
+++ Snack Attack/Pages/ItemsCartPage.xaml.cs
@@ -1,15 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Snack_Attack.ViewModels;
 
 namespace Snack_Attack.Pages;
 
 public partial class ItemsCartPage : ContentPage
 {
-    public ItemsCartPage()
+    public ItemsCartPage(ItemsCartViewModel _itemsCartViewModel)
     {
         InitializeComponent();
+        BindingContext = _itemsCartViewModel;
     }
 }

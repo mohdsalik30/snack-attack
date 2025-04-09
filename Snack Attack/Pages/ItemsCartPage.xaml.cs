28eb3360adfe064bbcd85b1784d4e6958b2b4c87
Mohammad Malik (malikm44) <malikm44@coventry.ac.uk>
2025-04-09 17:45:20 +0100

    added the disable funtion for back button in the cart

--- Snack Attack/Pages/ItemsCartPage.xaml.cs
+++ Snack Attack/Pages/ItemsCartPage.xaml.cs
@@ -1,12 +1,19 @@
+using Microsoft.Maui.Controls;
 using Snack_Attack.ViewModels;
 
 namespace Snack_Attack.Pages;
 
 public partial class ItemsCartPage : ContentPage
 {
+    private readonly ItemsCartViewModel itemsCartViewModel;
     public ItemsCartPage(ItemsCartViewModel _itemsCartViewModel)
     {
+        _itemsCartViewModel = itemsCartViewModel;
         InitializeComponent();
         BindingContext = _itemsCartViewModel;
+        
+        Shell.SetBackButtonBehavior(this, new BackButtonBehavior { IsVisible = false });
     }
+
+    
 }

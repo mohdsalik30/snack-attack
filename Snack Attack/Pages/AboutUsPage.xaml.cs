902dc771e43886fe9c9793e49e8cbbf945408d6c
Mohammad Malik (malikm44) <malikm44@coventry.ac.uk>
2025-04-10 08:42:12 +0100

    added about us page

---
+++ Snack Attack/Pages/AboutUsPage.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snack_Attack.Pages;
+
+public partial class AboutUsPage : ContentPage
+{
+    public AboutUsPage()
+    {
+        InitializeComponent();
+    }
+}

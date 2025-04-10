302c650d39e5a20216e5362e06e72830cb9f56bd
Mohammad Malik (malikm44) <malikm44@coventry.ac.uk>
2025-04-10 05:33:34 +0100

    added order placed text, check image, and home button in the cart which takes back to the home page. i also added an animationwhen place order clicked in cart page.

--- Snack Attack/Pages/OrderPlacedPage.xaml.cs
+++ Snack Attack/Pages/OrderPlacedPage.xaml.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Snack_Attack.Pages;
 
 public partial class OrderPlacedPage : ContentPage
@@ -11,5 +5,25 @@ public partial class OrderPlacedPage : ContentPage
     public OrderPlacedPage()
     {
         InitializeComponent();
+        AnimateElements();
     }
+
+    private  async void AnimateElements()
+    {
+        await CheckImage.FadeTo(1, 200);
+        await CheckImage.ScaleTo(1.2, 300, Easing.SpringOut);
+        await CheckImage.ScaleTo(1.0, 150, Easing.CubicOut);
+        
+        await OrderPlaced.FadeTo(1, 300);
+        await OrderPlaced.TranslateTo(0, 0, 300, Easing.CubicOut);
+        
+        await ReturnHome.ScaleTo(1, 300, Easing.CubicOut);
+        await ReturnHome.FadeTo(1, 300);
+        await ReturnHome.TranslateTo(0, 0, 300, Easing.CubicOut);
+    }
+    private async void ReturnHomeButton_Clicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync($"//{nameof(HomePage)}", animate: true); 
+    }
+
 }
